a65b6c922e757a3f4864d229503b4d404170a18f
Amaury Gutierrez <acoamaur@amazon.com>
2020-09-26 18:06:21 -0700

    Updates

---
+++ precache-manifest.a245f8fa38e54aec693f150f533d38ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2e743b0da755c38750c9af4e00f021",
+    "url": "/tejido/index.html"
+  },
+  {
+    "revision": "e001beb19f94cd99e945",
+    "url": "/tejido/static/css/main.37967d83.chunk.css"
+  },
+  {
+    "revision": "cbbc2a40902b878e4b85",
+    "url": "/tejido/static/js/2.80d3c918.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tejido/static/js/2.80d3c918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e001beb19f94cd99e945",
+    "url": "/tejido/static/js/main.1385ac91.chunk.js"
+  },
+  {
+    "revision": "d2384b9ae32cf423338d",
+    "url": "/tejido/static/js/runtime-main.f70abbef.js"
+  }
+]);
